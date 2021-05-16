84bb468eb31d9d03e9f3746258578670b8596951
CheonYoungJo <pknb213@naver.com>
2021-05-17 02:26:25 +0900

    Updated

--- src/main/kotlin/Type_Checks_and_automatic_casts.kt
+++ src/main/kotlin/Type_Checks_and_automatic_casts.kt
@@ -2,25 +2,28 @@ fun main() {
     // is 연산자는 인스턴트의 타입 표현식을 체크합니다. 만약 불변적인 지역 변수 또는 프로퍼티는
     fun getStringLength(obj: Any): Int?{
         if (obj is String) {
-            // `obj` is automatically cast to `String` in this branch
+            // obj is String일 경우 String으로 자동 형 변환이 됩니다.
             return obj.length
         }
-        // `obj` is still of type `Any` outside of the type-checked branch
+        // obj는 여전히 Any 타입 일 경우 수행.
         return null
     }
 
     // Or
     fun getStringLength2(obj: Any): Int? {
         if (obj !is String) return null
-        // `obj` is automatically cast to `String` in this bran
+        // obj !is String가 아닌 경우 String으로 자동 형 변환이 됩니다.
         return null
     }
     // Or even
     fun getStringLength3(obj: Any): Int? {
-        // `obj` is automatically cast to `String` on the right-hand side of `&&`
+        // `obj`는 &&의 오른쪽에 있는 String으로 자동 형 변환이됩니다.
         if (obj is String && obj.length > 0) {
             return obj.length
         }
         return null
     }
+    println(getStringLength("Hello"))
+    println(getStringLength2("Hi"))
+    println(getStringLength3("Welcome"))
 }
